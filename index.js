724fa86fb3c81c602fca3c7f3b46315264a6c47f
hudanwidzamil <hudanwidzamil@gmail.com>
2020-12-12 15:20:14 +0700

    local backend finished

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
 require('./passport-init');
@@ -10,15 +11,21 @@ require("dotenv").config();
 const Report = require('./Report');
 const Logging = require('./Logging');
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  credentials: true
+}));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use(cookieSession({
-    name: 'session-name',
-    keys: ['key1', 'key2']
+    name: 'session',
+    keys: ['key'],
+    maxAge: 24 * 60 * 60 * 100
 }))
+app.use(cookieParser());
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,42 +47,64 @@ app.get('/welcome', checkUserLoggedIn,(req,res)=> {
   res.send(`Wilkommen ${req.user.displayName}, ${req.user.emails[0].value}`);
 });
 
-app.get('/failed', (req,res)=> res.send('Failed to login'));
+app.get('/login/success', (req,res)=>{
+  if (req.user) {
+    res.json({
+      success: true,
+      message: "autentikasi berhasil",
+      user: req.user,
+      cookies: req.cookies
+    });
+  }
+});
+app.get('/login/failed', (req,res)=>{
+  res.status(401).json({
+    success: false,
+    message: "autentikasi gagal"
+  });
+});
 
 app.get('/login',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 app.get('/login/callback', 
-  passport.authenticate('google', { failureRedirect: '/failed' }),
+  passport.authenticate('google', { failureRedirect: '/login/failed' }),
   function(req, res) {
     // Successful authentication, redirect.
-    res.redirect('/welcome');
+    res.redirect(process.env.CLIENT_URL);
+    //res.redirect('/login/success');
   });
 
 app.get('/logout', (req, res) => {
     req.session = null;
     req.logout();
-    res.redirect('/');
+    res.redirect(process.env.CLIENT_URL);
 });
 
-app.post('/report',checkUserLoggedIn,(req,res)=>{
+app.post('/report', checkUserLoggedIn, async (req,res)=>{
   let log = new Logging({email:req.user.emails[0].value, date: Date.now, apicalled:'Post Report'});
   log.save();
-  let report = new Report({
-    laporan: req.body.laporan,
-    tanggal: Date.now,
-    lokasi: req.body.lokasi,
-    status: 'Laporan terkirim',
-    nama: req.user.displayName,
-    email: req.user.emails[0]
-  });
-  report.save();
-  res.sendStatus(201);
+  try {
+    let report = await new Report({
+      laporan: req.body.laporan,
+      lokasi: req.body.lokasi,
+      status: 'Laporan terkirim',
+      nama: req.user.displayName,
+      email: req.user.emails[0].value
+    });
+    report.save(function(err, newReport) {
+      console.log(newReport._id);
+    }); 
+    res.status(201).send(report);
+  } catch (error) {
+    res.json({message:error});
+  }
 });
 
-app.get('/report',checkUserLoggedIn, async (req,res)=>{
+app.get('/report', checkUserLoggedIn, async (req,res)=>{
   let log = new Logging({email:req.user.emails[0].value, date: Date.now, apicalled:'Get Report'});
   log.save();
+  console.log(req);
   try {
     const report = await Report.find();
     res.json(report);  
@@ -84,38 +113,38 @@ app.get('/report',checkUserLoggedIn, async (req,res)=>{
   }
 });
 
-app.get('/report/:id',checkUserLoggedIn, async (req,res)=>{
+app.get('/report/:_id', checkUserLoggedIn, async (req,res)=>{
   let log = new Logging({email:req.user.emails[0].value, date: Date.now, apicalled:'Get Report by id'});
   log.save();
   try {
-    const report = await Report.findById(req.params.id);
+    const idparam = mongoose.Types.ObjectId(req.params._id);
+    const report = await Report.findById(idparam);
     res.json(report);
   } catch (error) {
-    res.json({message:error});
+    res.json(null);
   }  
 });
 
-app.patch('/report/:id', checkUserLoggedIn, async (req,res)=>{
+app.patch('/report/:_id', checkUserLoggedIn, async (req,res)=>{
   let log = new Logging({email:req.user.emails[0].value, date: Date.now, apicalled:'Patch Report'});
   log.save();
   try {
     const patch = await Report.updateOne(
-      {id: req.params.id},
+      {_id: req.params._id},
       {$set:{status:req.body.status}},
       {upsert:true}
     );
-    res.json(patch);
-    res.sendStatus(201);
+    res.status(201).send(patch);
   } catch (error) {
     res.json({message:error});
   }
 });
 
-app.delete('/report/:id', checkUserLoggedIn, async (req,res)=>{
+app.delete('/report/:_id', checkUserLoggedIn, async (req,res)=>{
   let log = new Logging({email:req.user.emails[0].value, date: Date.now, apicalled:'Delete Report'});
   log.save();
   try {
-    const del = await Report.deleteOne({_id: req.params.id});
+    const del = await Report.deleteOne({_id: req.params._id});
     res.json(del);
   } catch (error) {
     res.json({message:error});
--- passport-init.js
+++ passport-init.js
@@ -13,7 +13,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
+    callbackURL: "/login/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     return cb(null, profile);
